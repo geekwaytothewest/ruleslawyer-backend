d01baf44527ca93738a0de5691a3a75e9cd134a2
Mattie Schraeder <mattie@mattie.lgbt>
2023-09-30 18:28:04 -0500

    user create and get works!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RouterModule } from '@nestjs/core';
+import { UserModule } from './modules/user.module';
+
+const routes = [
+  {
+    path: 'user',
+    module: UserModule,
+  },
+];
 
 @Module({
-  imports: [],
+  imports: [RouterModule.register(routes), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
