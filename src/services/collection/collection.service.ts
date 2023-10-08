67a338f05fc7aa53d81e426fd8276c4876490901
Mattie Schraeder <mattie@mattie.lgbt>
2023-10-08 02:19:18 -0500

    handle copies properly

--- src/services/collection/collection.service.ts
+++ src/services/collection/collection.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { parse } from 'csv-parse';
 import { Context } from '../prisma/context';
+import { Collection, Prisma } from '@prisma/client';
 
 @Injectable()
 export class CollectionService {
@@ -14,23 +15,82 @@ export class CollectionService {
 
   async importCollection(
     orgId: number,
-    name: string,
+    fields: any,
     csvData: Buffer,
     ctx: Context,
   ) {
-    const promise = new Promise(async (resolve) => {
-      const collection = await ctx.prisma.collection.create({
-        data: {
-          name: name,
-          organizationId: Number(orgId),
-        },
-      });
+    const promise = new Promise(async (resolve, reject) => {
+      if (!fields?.name) {
+        reject('missing name');
+        return;
+      }
+
+      if (fields?.type !== undefined) {
+        if (
+          fields?.type.value !== 'Door Prizes' &&
+          fields?.type.value !== 'Play and Win'
+        ) {
+          reject('invalid type');
+          return;
+        }
+
+        if (!fields?.conventionId) {
+          reject('missing convention id');
+          return;
+        }
+      }
+
+      let collection: Collection | null = null;
+
+      try {
+        collection = await ctx.prisma.collection.create({
+          data: {
+            name: fields.name.value,
+            organizationId: Number(orgId),
+          },
+        });
+      } catch (ex) {
+        if (ex instanceof Prisma.PrismaClientKnownRequestError) {
+          switch (ex.code) {
+            case 'P2002':
+              reject('a collection already exists with that name');
+              return;
+              break;
+          }
+        }
+
+        reject(ex.message);
+        return;
+      }
+
+      if (fields?.type?.value === 'Door Prizes') {
+        await ctx.prisma.convention.update({
+          where: {
+            id: Number(fields.conventionId.value),
+            organizationId: Number(orgId),
+          },
+          data: {
+            doorPrizeCollectionId: Number(collection?.id),
+          },
+        });
+      } else if (fields?.type?.value === 'Play and Win') {
+        await ctx.prisma.convention.update({
+          where: {
+            id: Number(fields.conventionId.value),
+            organizationId: Number(orgId),
+          },
+          data: {
+            playAndWinCollectionId: Number(collection?.id),
+          },
+        });
+      }
 
       let importCount = 0;
 
       parse(csvData, { delimiter: ',' }, async (error, records) => {
         if (error) {
-          return error;
+          reject(error);
+          return;
         }
 
         for (const r of records) {
@@ -52,7 +112,7 @@ export class CollectionService {
               winnable: false,
               collections: {
                 connect: {
-                  id: collection.id,
+                  id: Number(collection?.id),
                 },
               },
             },
@@ -61,7 +121,10 @@ export class CollectionService {
           importCount++;
         }
 
-        resolve(importCount);
+        resolve({
+          collectionId: collection?.id,
+          importCount: importCount,
+        });
       });
     });
 
@@ -71,12 +134,40 @@ export class CollectionService {
   async deleteCollection(id: number, ctx: Context) {
     const conventions = await ctx.prisma.convention.count({
       where: {
-        OR: [{ doorPrizeCollectionId: id }, { playAndWinCollectionId: id }],
+        OR: [
+          { doorPrizeCollectionId: Number(id) },
+          { playAndWinCollectionId: Number(id) },
+        ],
       },
     });
 
     if (conventions) {
-      return 'cannot delete a collection tied to a convention.';
+      return 'cannot delete a collection tied to a convention';
+    }
+
+    const copies = await ctx.prisma.copy.findMany({
+      where: {
+        collections: {
+          some: {
+            id: Number(id),
+          },
+        },
+      },
+      include: {
+        collections: {},
+      },
+    });
+
+    for (const c of copies) {
+      const count = c.collections.filter((c) => c.id !== Number(id)).length;
+
+      if (!count) {
+        await ctx.prisma.copy.delete({
+          where: {
+            id: c.id,
+          },
+        });
+      }
     }
 
     await ctx.prisma.collection.delete({ where: { id: Number(id) } });
