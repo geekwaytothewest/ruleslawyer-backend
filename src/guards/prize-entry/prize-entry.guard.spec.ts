0d3a66d4d002652b0a0463ec80c423bfc6a4598f
Mattie Schraeder <mattie@mattie.lgbt>
2023-11-23 22:52:12 -0600

    prize entry guard

---
+++ src/guards/prize-entry/prize-entry.guard.spec.ts
@@ -0,0 +1,7 @@
+import { PrizeEntryGuard } from './prize-entry.guard';
+
+describe('PrizeEntryGuard', () => {
+  it('should be defined', () => {
+    expect(new PrizeEntryGuard()).toBeDefined();
+  });
+});
